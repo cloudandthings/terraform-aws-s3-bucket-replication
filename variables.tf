324ea6482baab4c8d959d1cb9e59f4b936e0a947
Bjorn Olsen <bjorn@cloudandthings.io>
2025-06-24 12:12:08 +0200

    fix: Make tags an optional variable

--- variables.tf
+++ variables.tf
@@ -57,11 +57,6 @@ variable "replication_configuration" {
   )
 }
 
-variable "tags" {
-  description = "Map of additional tags to assign to created resources. If configured with a provider `default_tags` configuration block present, tags with matching keys will overwrite those defined at the provider-level."
-  type        = map(string)
-}
-
 ######################################################################
 ## Optional
 ######################################################################
@@ -75,5 +70,11 @@ variable "create_iam_resources" {
 variable "replication_role_arn" {
   description = "IAM Role ARN for replication role."
   type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "Map of additional tags to assign to created resources. If configured with a provider `default_tags` configuration block present, tags with matching keys will overwrite those defined at the provider-level."
+  type        = map(string)
   default     = null
 }
