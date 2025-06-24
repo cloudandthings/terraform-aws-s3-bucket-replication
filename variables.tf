152dbbaf35a08b278d70a5385244a4bccf4e616c
Bjorn Olsen <bjorn@cloudandthings.io>
2025-06-24 14:06:33 +0200

    f

--- variables.tf
+++ variables.tf
@@ -76,5 +76,5 @@ variable "replication_role_arn" {
 variable "tags" {
   description = "Map of additional tags to assign to created resources. If configured with a provider `default_tags` configuration block present, tags with matching keys will overwrite those defined at the provider-level."
   type        = map(string)
-  default     = null
+  default     = {}
 }
